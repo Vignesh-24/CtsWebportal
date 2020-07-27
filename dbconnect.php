4d9b2b637f8f43b21e76f7abadc9cd00feb2ec17
Vignesh K G <59288428+Vignesh-24@users.noreply.github.com>
2020-07-27 19:36:54 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
     
-    DEFINE ('DB_USER','id12680429_root');
+    DEFINE ('DB_USER','root');
     DEFINE ('DB_HOST','localhost');
-    DEFINE ('DB_PSWD','focus');
-    DEFINE ('DB_NAME','id12680429_ctsclub');
+    DEFINE ('DB_PSWD','password');
+    DEFINE ('DB_NAME','name');
 
     $scon=mysqli_connect(DB_HOST,DB_USER,DB_PSWD,DB_NAME);
 
